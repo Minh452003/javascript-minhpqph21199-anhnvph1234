b1bac3f81430fff0f6acff8bb94f83b5ba36f923
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-01-15 14:48:22 +0700

    nop lab2

---
+++ lab1/onluyen/circuiting.js
@@ -0,0 +1,11 @@
+
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
